d60627450437c65c6bb1baa960219dd77d3e14a5
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-06-15 20:49:03 -0500

    add a new class

---
+++ Todoey/Data Model/Item .swift
@@ -0,0 +1,14 @@
+//
+//  Item .swift
+//  Todoey
+//
+//  Created by Jianfeng Yang on 6/15/18.
+//  Copyright © 2018 Jianfeng Yang. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
